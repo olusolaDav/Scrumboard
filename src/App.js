63cfeb3547244043a4c4a856dd0b3984d189bf17
Akinbode Olusola <akinbodeolusola9611@gmail.com>
2022-09-14 12:14:14 +0100

    lab 4

--- src/App.js
+++ src/App.js
@@ -1,99 +1,14 @@
 import React, {Component} from 'react'
 import './App.css';
+import Devs from './Devs';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Linuxjobber",
-      skill: "",
-      session: "",
-      isBackgroundRed: true,
-    };
-  }
-
-  handleChange = (e) => {
-    const value = e.target.value;
-    const name = e.target.name
-    this.setState({
-      ...this.state,
-      [name]: value
-    });
-    console.log(`${value}`);
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(
-      `this form submited ${this.state.skill} and ${this.state.session}`
-    );
-     //const name = e.target.name;
-     const value = e.target.value;
-     this.setState((values) => ({ ...values, [value]: ''}));
-  };
-
-  handleClick = () => {
-    console.log("Button click");
-  };
-
-  handleMouseOver = () => {
-    console.log("You just mouse over a button");
-  };
-
-  handleMouseDown = () => {
-    console.log("You just mouse Down a button");
-  };
-
-  handleKeyUp = () => {
-    this.setState({ isBackgroundRed: false });
-    console.log("The b-color changed to blue as you key up");
-  };
-
-  handleKeyDown = () => {
-    this.setState({ isBackgroundRed: true });
-    console.log("The b-color changed to red as you keydown");
-  };
-
+  c
   render() {
     return (
-      <div
-        className={`App ${
-          this.state.isBackgroundRed ? "background-red" : "background-blue"
-        }`}
-      >
-        <h1>Greeting from {this.state.name}</h1>
-        <p>
-          We welcome all intern to {this.state.session} {this.state.skill}{" "}
-          classs
-        </p>
-        <button onClick={this.handleClick}>Onclick</button>
-        <button onMouseOver={this.handleMouseOver}>mouseover</button>
-        <button onMouseDown={this.handleMouseDown}>  Mousedown</button>
-        Type to change background:{" "}
-        <input
-          type="text"
-          id="fname"
-          onKeyUp={this.handleKeyUp}
-          onKeyDown={this.handleKeyDown}
-        />
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="enter session here"
-            name="session"
-            value={this.state.session}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            placeholder="enter skill here"
-            name="skill"
-            value={this.state.skill}
-            onChange={this.handleChange}
-          />
-          <button type="submit">SUBMIT</button>
-        </form>
+      <div className='app'>
+        <Devs name= "Olusola" skill="React"/>
       </div>
     );
   }
--- src/Devs.jsx
+++ src/Devs.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 class Devs extends Component {
     render() { 
+
+        const {name, skill} = this.props
+
+        console.log(name, skill)
         return (
-            <div>Hello class! :)</div>
+          <div>
+            <h1>Hello class! :)</h1>
+            <ul>
+                <li>Name: {name}</li>
+                <li>Skill: {skill}</li>
+            </ul>
+          </div>
         );
     }
 }
