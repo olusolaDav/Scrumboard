0c5cac0e84b35739cb0b7a9e1c381c0ee58719ad
Akinbode Olusola <akinbodeolusola9611@gmail.com>
2022-09-23 04:51:55 +0100

    Add drag and drop feature

---
+++ src/component/static/task.js
@@ -0,0 +1,29 @@
+export const taskList1 = [
+  "Read bible",
+  "Do an hour worship",
+  "Do quiet time",
+  "Surf the internet",
+  "Review codes and debug",
+  "Read a book",
+  "Write an article",
+];
+export const taskList2 = [
+  "Build chatscrum",
+  "Build trello app",
+  "Go shopping",
+  "Sleep all night",
+  "Play call to duty",
+];
+
+
+// export const taskList2 = {
+//   items: [
+//     { id: 1, item: "build chatscrum" },
+//     { id: 2, item: "build trello app" },
+//     { id: 3, item: "go shopping" },
+//     { id: 4, item: "sleep all night" },
+//     { id: 5, item: "play call a duty" },
+//   ],
+// };
+
+
